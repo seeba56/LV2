0d130d5e59e0295456dc1442f74fce7ed8e11420
seeba56 <62594505+seeba56@users.noreply.github.com>
2020-04-17 15:40:09 +0200

    Add files via upload

---
+++ Die.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LaboratorijskeVježbe2
+{
+    class Die
+    {
+        private int numberOfSides;
+        private RandomGenerator randomGenerator;
+        public Die(int numberOfSides)
+        {
+            this.numberOfSides = numberOfSides;
+            this.randomGenerator = RandomGenerator.GetInstance();
+        }
+
+        //public Die(int numberOfSides, Random random)
+        //{
+        //    this.numberOfSides = numberOfSides;
+        //    this.randomGenerator = random;
+        //}
+
+        public int Roll()
+        {
+            //int rolledNumber = randomGenerator.Next(1, numberOfSides + 1);
+            int rolledNumber = randomGenerator.NextInt(1, numberOfSides + 1);
+            return rolledNumber;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LaboratorijskeVježbe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceRoller diceRoller = new DiceRoller();
+            Random randomGenerator = new Random();
+            int numberOfDice = 20;
+            for(int i = 0; i<numberOfDice; i++)
+            {
+                //diceRoller.InsertDie(new Die(6, randomGenerator)); predaja generatora 2. zadatak
+                diceRoller.InsertDie(new Die(6));
+            }
+            diceRoller.RollAllDice();
+            diceRoller.GetRollingResults();
+
+            foreach(int result in diceRoller.GetRollingResults())
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
